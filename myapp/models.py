37aaf00e0149c9f87d9619b66b63780bf6782264
alphy1 <alphiya01@mail.ru>
2022-06-10 01:14:19 +0300

    feat: create `Customer` model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+# todo: Understand what not standard user means
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return f"Customer: {self.name}"
+
