dbf33abaddce97d42612c4c0b9fa9d52a9f8c263
alphy1 <alphiya01@mail.ru>
2022-06-10 12:08:55 +0300

    feat: add `order_id` to `OrderItem`

--- myapp/models.py
+++ myapp/models.py
@@ -46,6 +46,7 @@ class Product(models.Model):
 
 
 class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.FloatField()
